100e9a7f2dfcb9e42410eee0db7d2b7dfbeafdb9
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2023-07-28 18:51:26 +0600

    Google Authentication login is added

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,16 @@ const HomePage = () => {
       <Head>
         <title>Next Auth</title>
       </Head>
-      <h1 style={{ textAlign: "center", marginTop: "10%" }}>
+      <h1 gold-1 style={{ textAlign: "center", marginTop: "10%" }}>
         Welcome To Next Auth Home Page
       </h1>
       <h2 style={{ textAlign: "center" }}>
-        Logged in user: {session?.user?.name}
+        Logged in user:{" "}
+        <span style={{ color: "#ff4d4f" }}>{session?.user?.name}</span>
+      </h2>
+      <h2 style={{ textAlign: "center" }}>
+        Logged in user email:{" "}
+        <span style={{ color: "#ff4d4f" }}>{session?.user?.email}</span>
       </h2>
     </div>
   );
